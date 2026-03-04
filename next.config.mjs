231b8501ca5a6b01e3748b1a4fc21ba053cff020
jirangi <121839308+jirangi@users.noreply.github.com>
2026-03-05 01:44:46 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 정적 사이트로 배포
-  basePath: '/workout_ads', // 리포지토리 이름과 일치시켜야 함
+  output: 'export',
+  basePath: '/workout_ads',
+  assetPrefix: '/workout_ads/',
   images: {
-    unoptimized: true, // GitHub Pages는 이미지 최적화를 지원하지 않음
+    unoptimized: true,
   },
 };
 
